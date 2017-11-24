bba41f5cdf5a9e3529e750ebdb33f2ed1517b9b6
MrOrange1992 <felix.rauchenwald@edu.fh-joanneum.at>
2017-11-24 11:05:21 +0100

    Renamed QuarterSpendings to MediaAnalysisLinearRegression Modified csv fields Created Custom Schema for reading data from csv to DataFrame Implemented Linear Regression Model for MT Data

---
+++ src/main/scala/MedienTransparenz/MediaAnalysisLinearRegression.scala
@@ -0,0 +1,88 @@
+package MedienTransparenz
+
+import org.apache.log4j._
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+
+
+object MediaAnalysisLinearRegression
+{
+
+  def main(args: Array[String]): Unit =
+  {
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    // Spark Context, using all Cores of local machine
+    val spark = SparkSession.builder.appName("MediaAnalysisLinearRegression").master("local[*]").getOrCreate()
+
+
+    //Custom Schema for data frame to cast numeric fields
+    val customSchema = StructType(Array(
+      StructField("RECHTSTRÄGER", StringType, true),
+      StructField("QUARTAL", IntegerType, true),
+      StructField("BEKANNTGABE", IntegerType, true),
+      StructField("LEERMELDUNG", IntegerType, true),
+      StructField("MEDIUM_MEDIENINHABER", StringType, true),
+      StructField("EURO", FloatType, true)
+    ))
+
+    /*
+     csv sanitations ~lukas:
+      deleted all unused columns
+      replaced all 'null' with ''
+      replaced all '""' with ''
+   */
+    val data = spark.sqlContext.read
+      .format("com.databricks.spark.csv")
+      .option("header", "true")
+      .schema(customSchema)
+      .load("./data/MedienTransparenz-20123-20171.csv")
+      .cache()
+    //data.printSchema()
+    //data.show()
+
+    //Filter null fields
+    //BAKANNTGABE:
+      //4: Förderungen und Programmentgeld
+      //2: Bekanntgabepflichtige Aufträge
+    val filteredData = data.filter(data("LEERMELDUNG") === "0").filter(data("BEKANNTGABE") =!= "4")
+
+
+    // select desired fields and create label column in DS
+    val mtDataDF = filteredData.select(data("QUARTAL").cast(FloatType).as("label"), data("EURO").cast(FloatType))
+
+
+    // features column must be of type VectorAssembler
+    // in our case it is just one field ("EURO)
+    val assembler = new VectorAssembler().setInputCols(Array("EURO")).setOutputCol("features")
+
+    // prepare dataframe
+    val dataLR = assembler.transform(mtDataDF).select("label", "features")
+
+
+    // LINEAR REGRESSION
+    val lr = new LinearRegression()
+
+    val lrModel = lr.fit(dataLR)
+
+    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+
+    val trainingSummary = lrModel.summary
+
+    println(s"numIterations: ${trainingSummary.totalIterations}")
+    println(s"objectiveHistory: ${trainingSummary.objectiveHistory.toList}")
+
+    trainingSummary.residuals.show()
+
+    println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
+    println(s"MSE: ${trainingSummary.meanSquaredError}")
+    println(s"r2: ${trainingSummary.r2}")
+
+  }
+
+}
+
+
