5915c564f165fe50d55bc569ba104e2c14966315
MrOrange1992 <felix.rauchenwald@edu.fh-joanneum.at>
2017-11-30 15:11:00 +0100

    Work on MT Data: correlation of RT and Medium spending with RDD

--- src/main/scala/MedienTransparenz/MediaAnalysisLinearRegression.scala
+++ src/main/scala/MedienTransparenz/MediaAnalysisLinearRegression.scala
@@ -5,6 +5,10 @@ import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.regression.LinearRegression
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions.{stddev_pop, stddev_samp}
+import org.apache.spark.rdd
+import org.apache.spark.rdd.RDD
+
 
 
 object MediaAnalysisLinearRegression
@@ -50,6 +54,50 @@ object MediaAnalysisLinearRegression
       //2: Bekanntgabepflichtige Aufträge
     val filteredData = data.filter(data("LEERMELDUNG") === "0").filter(data("BEKANNTGABE") =!= "4")
 
+    val quarterGroups = filteredData.groupBy("QUARTAL")
+
+    //Mittelwert für Ausgaben im Quartal gesamt
+    //quarterGroups.mean("EURO").sort("QUARTAL").show()
+
+    //Standardabweichung der Ausgaben pro Quartal
+    //filteredData.groupBy("QUARTAL").agg(stddev_pop("EURO")).sort("QUARTAL").show()//.describe().show()
+
+
+    val countRT = filteredData.groupBy("RECHTSTRÄGER").count()
+
+
+    val groupedRT = countRT.filter(countRT("count") > 10)
+
+
+    val keySet = filteredData.select("RECHTSTRÄGER").groupBy("RECHTSTRÄGER").count()//.distinct.collect//.flatMap(_.toSeq)
+
+    //val groupedMedia = filteredData.groupBy("MEDIUM_MEDIENINHABER")
+
+    val mediaRDD =  filteredData.rdd
+
+
+    // FRAGE
+    val mediaMap = mediaRDD.map( r => (r(0), List((r(4), r(5))))).reduceByKey((a, b) => a ++ b)//if (a.head._1 == b.head._1) a else a ++ b)
+
+
+    val pairs = mediaRDD.map(s => (s(0), 1))
+
+
+
+    //val mediaMap = pairs.reduceByKey((x,y) => x - y)
+
+
+
+    //pairs foreach println
+
+    //println(mediaMap.count())
+
+
+    //mediaRDD foreach println
+
+    type mediaMap = RDD[(String, List[(String, Float)])]
+
+    /*
 
     // select desired fields and create label column in DS
     val mtDataDF = filteredData.select(data("QUARTAL").cast(FloatType).as("label"), data("EURO").cast(FloatType))
@@ -65,20 +113,22 @@ object MediaAnalysisLinearRegression
     // LINEAR REGRESSION
     val lr = new LinearRegression()
 
+    //model
     val lrModel = lr.fit(dataLR)
 
-    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+    //MODEL SUMMARY
+    println(s"Coefficients: ${lrModel.coefficients} \nIntercept: ${lrModel.intercept}")
 
     val trainingSummary = lrModel.summary
-
-    println(s"numIterations: ${trainingSummary.totalIterations}")
-    println(s"objectiveHistory: ${trainingSummary.objectiveHistory.toList}")
-
+    //println(s"numIterations: ${trainingSummary.totalIterations}")
+    //println(s"objectiveHistory: ${trainingSummary.objectiveHistory.toList}")
     trainingSummary.residuals.show()
 
     println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
     println(s"MSE: ${trainingSummary.meanSquaredError}")
     println(s"r2: ${trainingSummary.r2}")
+
+    */
 
   }
 
